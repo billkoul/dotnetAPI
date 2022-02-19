8fe30ebdf4eb229909ba2f762e454858442a309e
Vasileios Kouliaridis <billkoul8@gmail.com>
2022-02-19 07:51:38 +0200

    Update CustomErrorCodes.cs
    cleanup

--- API/Config/CustomErrorCodes.cs
+++ API/Config/CustomErrorCodes.cs
@@ -2,17 +2,13 @@
 {
 	public class CustomErrorCodes
 	{
-        public string InvalidVehicle { get; set; }
-        public string InvalidDates { get; set; }
         public string InvalidJson { get; set; }
         public string InvalidRequest { get; set; }
 
         public CustomErrorCodes()
         {
-            InvalidVehicle = @"ERR01";
-            InvalidDates = @"ERR02";
-            InvalidJson = @"ERR03";
-            InvalidRequest = @"ERR04";
+            InvalidJson = @"ERR01";
+            InvalidRequest = @"ERR02";
         }
     }
 }
