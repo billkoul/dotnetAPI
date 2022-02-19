d1beb2813c02e72567037ac20cb84af42d4c4b64
Vasileios Kouliaridis <billkoul8@gmail.com>
2022-02-19 07:37:08 +0200

    Update UploadRepository.cs
    cleanup

--- Infrastructure/Repositories/Implementations/UploadRepository.cs
+++ Infrastructure/Repositories/Implementations/UploadRepository.cs
@@ -19,23 +19,5 @@ namespace Infrastructure.Repositories
             ConnectionString = connectionString;
             TableName = tableName;
         }
-
-        public override async Task<IEnumerable<T>> Find<T>(int gseId)
-        {
-            IEnumerable<T> results;
-
-            using (var connection = new NpgsqlConnection(ConnectionString))
-            {
-                connection.Open();
-                var sql = $@"SELECT 
-							    *				
-							FROM {TableName}
-                            WHERE ACTIVE=1 " + (gseId == -1 ? "" : " AND GseId = @GseId");
-
-                results = await connection.QueryAsync<T>(sql, new { GseId = gseId });
-            }
-
-            return results;
-        }
     }
 }
